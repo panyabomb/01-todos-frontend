5ef26d5a939f8bc04e7990b4499e948d9675573c
panyabomb <panya_bomb@hotmail.com>
2018-01-25 18:44:33 +0700

    challenge7 finished

--- README.md
+++ README.md
@@ -45,7 +45,8 @@ For a detailed explanation on how things work, check out the [guide](http://vuej
 * [x] Challenge 4
 * [x] Challenge 5
 * [x] Challenge 6
-* [ ] Challenge 7
+* [x] Challenge 7
+* [ ] Challenge 8
 * [ ] Challenge 8
 
 ถ้ามีข้อสงสัยหรือติดปัญหาสามารถเปิด Issue ขึ้นมาใหม่ได้เลย
--- src/components/VisibilityInput.vue
+++ src/components/VisibilityInput.vue
@@ -3,15 +3,15 @@
     <b-field class="is-pulled-right">
       <b-radio-button v-model="visibility"
         native-value="all">
-        <span>All</span>
+        <span>All {{counting}}</span>
       </b-radio-button>
       <b-radio-button v-model="visibility"
         native-value="active">
-        <span>Active</span>
+        <span>Active {{countActive}}</span>
       </b-radio-button>
       <b-radio-button v-model="visibility"
         native-value="completed">
-        <span>Completed</span>
+        <span>Completed {{countCompleted}}</span>
       </b-radio-button>
     </b-field>
     <button class="button is-danger is-focused" @click="deleteCompleted()">Delete All completed item</button>
@@ -20,9 +20,10 @@
 
 <script>
 import { store } from '@/store'
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 export default {
   computed: {
+    ...mapGetters(['todos']),
     visibility: {
       get: function () {
         return store.state.visibility
@@ -30,6 +31,15 @@ export default {
       set: function (newValue) {
         store.dispatch('changeVisibility', newValue)
       }
+    },
+    counting () {
+      return this.todos.length
+    },
+    countActive () {
+      return this.todos.filter((todo) => todo.completed === false).length
+    },
+    countCompleted () {
+      return this.todos.filter((todo) => todo.completed === true).length
     }
   },
   methods: {
